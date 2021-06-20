f25653209102502564696bc5c47811630945cf13
Denis Koljada <denis.koljada@campus.tu-berlin.de>
2021-06-20 22:29:25 +0200

    generalisation and fixes for graphql function

--- functions/graphql-handler/mongodb.go
+++ functions/graphql-handler/mongodb.go
@@ -2,7 +2,9 @@ package function
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -13,23 +15,37 @@ type mongoDB struct {
 	users   *mongo.Collection
 }
 
-// Connect
-func connectDB() mongoDB {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+// connect to the database
+func connectDB(ctx context.Context, db string, collection string) mongoDB {
+	if len(db) == 0 || len(collection) == 0 {
+		log.Fatal("Database and collection need to be specified")
+	}
+	// check mongodb value
+	mongoUrl, present := os.LookupEnv("mongo_url")
+	if !present {
+		log.Println("MongoDB url not found, defaulting to namespace")
+		mongoUrl = "openfaas-db-mongodb" //default mongodb kubernetes namespace if no env passed
+	}
+	uri := fmt.Sprintf("mongodb://%s", mongoUrl)
+	// attempt connecting to mongodb
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
 		log.Fatal(err)
 	}
+	// attempt performing a ping to check if database working
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Connected to MongoDB")
+	// return a session and the collection of the mongodb instance
 	return mongoDB{
 		session: client,
-		users:   client.Database("demo").Collection("users"),
+		users:   client.Database(db).Collection(collection),
 	}
 }
 
-// Disconnect
+// disconnect and close the session
 func (db mongoDB) closeDB() {
 	err := db.session.Disconnect(context.Background())
 	if err != nil {
