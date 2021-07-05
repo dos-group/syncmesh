5276019ebe9df0d0e8830e99573d4b09fa5ac322
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-07-05 15:16:52 +0200

    add python

--- infrastructure/startup.sh
+++ infrastructure/startup.sh
@@ -39,7 +39,7 @@ sudo systemctl restart faasd
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb-org python3.6
 
 
 sleep 30
