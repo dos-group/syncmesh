e7d36fd373b08027602d0770f2bf67502379735c
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-07-07 01:20:22 +0200

    fix invalid date

--- evaluation/http_traffic.py
+++ evaluation/http_traffic.py
@@ -14,7 +14,7 @@ def main(args):
     parser.add_argument('-d', '--delay', help="Time delay in seconds", default=60, type=int)
     parser.add_argument('-f', '--file', help="IP list file", default='ips.txt', type=str)
     parser.add_argument('-l', '--limit', help="Document limit per node", default=10, type=int)
-    parser.add_argument('--start_time', help="Start time for the query", default="2017-06-31T00:00:00Z", type=str)
+    parser.add_argument('--start_time', help="Start time for the query", default="2017-06-30T00:00:00Z", type=str)
     parser.add_argument('--end_time', help="End time for the query", default="2017-08-01T00:00:00Z", type=str)
 
     args = parser.parse_args(args)
@@ -26,6 +26,9 @@ def main(args):
         ip_list = ips.copy()
         ip_list.remove(ip)
         url = "http://" + ip + "/function/syncmesh-fn"
+        print(get_request_body(limit=args.limit, start_time=args.start_time,
+                                                     end_time=args.end_time,
+                                                     external_nodes_list=ip_list))
         r = requests.post(url, json=get_request_body(limit=args.limit, start_time=args.start_time,
                                                      end_time=args.end_time,
                                                      external_nodes_list=ip_list))
