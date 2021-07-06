7fe6b4c3a985fd70a67070b6b3102c1c2d5a7a3b
Kevin Kreutz <kreutz@campus.tu-berlin.de>
2021-07-06 19:34:28 +0200

    modified aggregation script

--- baseline/aggregate_sensor_data.sh
+++ baseline/aggregate_sensor_data.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 username="kreutz"
-MASTER_IP="10.132.0.2"
+MASTER_IP="34.78.87.225"
+MASTER_IP_intern="10.132.0.2"
+PORT="2784"
 
 for i in "$@"
 do
-    ssh $i "scp data.csv $MASTER_IP:~"
-    mongoimport --type csv -d database_test -c test --headerline --drop data.csv
+    ssh $i "mongoimport -h $MASTER_IP:$PORT --type csv -d database_test -c test --headerline --drop import.csv"
 done
