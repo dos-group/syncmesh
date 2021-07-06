b820bdc4aadda502de62c105b8b09be7fcbdc337
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-07-07 01:20:03 +0200

    add interal ip

--- baseline/aggregate_sensor_data.sh
+++ baseline/aggregate_sensor_data.sh
@@ -6,6 +6,6 @@ MASTER_IP_intern="10.2.0.3"
 PORT="27017"
 
 while IFS=, read -u10 -r externalIP internalIP; do 
-    echo "SHH into $externalIP"
+    echo "SHH into $externalIP ($internalIP)"
     ssh -o StrictHostKeyChecking=no $externalIP "mongoimport -h $MASTER_IP_intern:$PORT --type csv -d database_test -c test --headerline --drop /import.csv"
 done 10< ips.txt
