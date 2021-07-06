914f01459e02c4cb238c21d3f129713b273cb994
Kevin Kreutz <kreutz@campus.tu-berlin.de>
2021-07-06 19:51:13 +0200

    modified aggreagation function for baseline

--- baseline/aggregate_sensor_data.sh
+++ baseline/aggregate_sensor_data.sh
@@ -5,7 +5,6 @@ MASTER_IP="34.78.87.225"
 MASTER_IP_intern="10.132.0.2"
 PORT="2784"
 
-for i in "$@"
-do
-    ssh $i "mongoimport -h $MASTER_IP:$PORT --type csv -d database_test -c test --headerline --drop import.csv"
-done
+while IFS=, read -r externalIP; do 
+    ssh $externalIP "mongoimport -h $MASTER_IP_intern:$PORT --type csv -d database_test -c test --headerline --drop import.csv"
+done < ips.txt
