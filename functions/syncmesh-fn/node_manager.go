0384c6d4917bd0d0cf14dbe8cd33bbdbc95e80d5
Denis Koljada <denis.koljada@campus.tu-berlin.de>
2021-07-14 21:44:39 +0200

    added CRUD operations for external nodes in syncmesh-fn

---
+++ functions/syncmesh-fn/node_manager.go
@@ -0,0 +1,92 @@
+package function
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+const MetaDB = "syncmesh_meta"
+const NodeCollection = "nodes"
+
+type SyncmeshNode struct {
+	Address string  `bson:"address" json:"address"`
+	Lat     float64 `bson:"lat" json:"lat,omitempty"`
+	Lon     float64 `bson:"lon" json:"lon,omitempty"`
+}
+
+// getSyncmeshDB as an instance of the meta node database
+func getSyncmeshDB(ctx context.Context) mongoDB {
+	// connect to mongodb external node collection
+	db = connectDB(ctx, MetaDB, NodeCollection)
+	defer db.closeDB()
+	return db
+}
+
+// getSyncmeshNodes and their information currently stored in the meta db
+func (db mongoDB) getSyncmeshNodes() []SyncmeshNode {
+	var err error
+	var nodes []SyncmeshNode
+
+	// query all external nodes saved in database
+	ctx, _ := context.WithTimeout(context.Background(), 90*time.Second)
+	cur, err := db.collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for cur.Next(ctx) {
+		var node SyncmeshNode
+		err = cur.Decode(&node)
+		if err != nil {
+			return nodes
+		}
+		nodes = append(nodes, node)
+	}
+	if err = cur.Err(); err != nil {
+		return nodes
+	}
+	err = cur.Close(ctx)
+	return nodes
+}
+
+// updateCreateNode updates an external node entry or creates a new one, if it does not exist
+func (db mongoDB) updateCreateNode(node SyncmeshNode, _id string) (interface{}, error) {
+	var err error
+	var updatedNode SyncmeshNode
+
+	id, err := primitive.ObjectIDFromHex(_id)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 90*time.Second)
+	filter := bson.M{"_id": id}
+	update := bson.D{{"$set", node}}
+	opts := options.FindOneAndUpdate().SetUpsert(true) // insert if no node with id found
+	err = db.collection.FindOneAndUpdate(ctx, filter, update, opts).Decode(&updatedNode)
+	if err != nil {
+		return nil, err
+	}
+	return updatedNode, nil
+}
+
+// deleteNode using its id
+func (db mongoDB) deleteNode(_id string) (interface{}, error) {
+	var node SyncmeshNode
+	var err error
+
+	id, err := primitive.ObjectIDFromHex(_id)
+	if err != nil {
+		return nil, err
+	}
+	q := bson.M{"_id": id}
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err = db.collection.FindOneAndDelete(ctx, q).Decode(&node)
+	if err != nil {
+		return nil, err
+	}
+	return node, nil
+}
