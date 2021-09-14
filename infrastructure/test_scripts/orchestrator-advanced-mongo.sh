ff592a407cd9aebbc58f52465ba7c74bdfdc3ed0
vottz <kevin.parkkreutz@gmail.com>
2021-09-14 23:02:23 +0200

    changed orchestrator to handle aggregation

--- infrastructure/test_scripts/orchestrator-advanced-mongo.sh
+++ infrastructure/test_scripts/orchestrator-advanced-mongo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SERVER_IP=$(</server.txt)
-CLIENT_IP="$(</client.txt)"
-SEPERATOR_IP="$(</seperator.txt)"
+SERVER_IP=$(</server.txt) #10.1.0.3
+CLIENT_IP="$(</client.txt)" #10.1.0.2
+SEPERATOR_IP="$(</seperator.txt)" #92.60.39.199
 PORT="27017"
 
 REPETITIONS=20
@@ -98,75 +98,70 @@ done
 
 
 
-seperate
+#seperate
+
+
+
+
+# Upload all data 
+uploadData 1
+uploadData 7
+uploadData 14
+uploadData 30
+
+sleep $SLEEP_TIME
+#seperate
 
 # Collect
-echo "Scenario: Collect - 1 day"
+
 # Write Data Once to central database
-#uploadData 1
-queryDataCollect "2017-07-31T00:00:00Z"
+# echo "Scenario: Collect - 1 day"
+# queryDataCollect "2017-07-31T00:00:00Z"
+
+# echo "Scenario: Collect - 7 day"
+# queryDataCollect "2017-07-24T00:00:00Z"
 
 
-sleep $SLEEP_TIME
-seperate
+# sleep $SLEEP_TIME
+# seperate
 
-echo "Scenario: Collect - 7 day"
+# echo "Scenario: Collect - 14 day"
+# queryDataCollect "2017-07-17T00:00:00Z"
 
-#uploadData 7
-queryDataCollect "2017-07-24T00:00:00Z"
+# sleep $SLEEP_TIME
+# seperate
 
+# echo "Scenario: Collect - 30 day"
+# queryDataCollect "2017-06-30T00:00:00Z"
 
-sleep $SLEEP_TIME
-seperate
-
-echo "Scenario: Collect - 14 day"
-
-#uploadData 14
-queryDataCollect "2017-07-17T00:00:00Z"
-
-sleep $SLEEP_TIME
-seperate
-
-echo "Scenario: Collect - 30 day"
-
-uploadData 30
-queryDataCollect "2017-06-30T00:00:00Z"
-
-sleep $SLEEP_TIME
-seperate
+# sleep $SLEEP_TIME
+# seperate
 
 
 # Aggregate
-echo "Scenario: Aggregate - 1 day"
 # Write Data Once to central database
-#uploadData 1
+echo "Scenario: Aggregate - 1 day"
 queryDataAggregate "2017-07-31T00:00:00Z"
 
 
 sleep $SLEEP_TIME
-seperate
+#seperate
 
 echo "Scenario: Aggregate - 7 day"
-
-#uploadData 7
 queryDataAggregate "2017-07-24T00:00:00Z"
 
 
 sleep $SLEEP_TIME
-seperate
+#seperate
 
 echo "Scenario: Aggregate - 14 day"
-
-#uploadData 14
 queryDataAggregate "2017-07-17T00:00:00Z"
 
 sleep $SLEEP_TIME
-seperate
+#seperate
 
 echo "Scenario: Aggregate - 30 day"
-
-#uploadData 30
 queryDataAggregate "2017-06-30T00:00:00Z"
 
 sleep $SLEEP_TIME
-seperate
+#seperate
