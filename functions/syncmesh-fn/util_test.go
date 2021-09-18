024b898b2383297d6feadd7023e74c15ffa3b7fd
Denis Koljada <denis.koljada@campus.tu-berlin.de>
2021-09-18 11:59:50 +0200

    added more util tests

--- functions/syncmesh-fn/util_test.go
+++ functions/syncmesh-fn/util_test.go
@@ -21,3 +21,44 @@ func TestCalculateNodeDistance(t *testing.T) {
 		t.Fail()
 	}
 }
+
+// TestFindOwnNode tests the method of finding the own node in a list of nodes and returning it
+func TestFindOwnNode(t *testing.T) {
+	nodeList := []SyncmeshNode{{OwnNode: false}, {OwnNode: true}, {OwnNode: false}}
+	err, ownNode, externalNodes := findOwnNode(nodeList)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	t.Log(externalNodes)
+	if len(externalNodes) != 2 || !ownNode.OwnNode {
+		t.Fail()
+	}
+	noOwnNodesList := []SyncmeshNode{{OwnNode: false}, {OwnNode: false}, {OwnNode: false}}
+	err, _, _ = findOwnNode(noOwnNodesList)
+	if err != nil {
+		t.Log(err)
+	} else {
+		t.Fail()
+	}
+}
+
+// TestCalculateSensorAverages computes the averages count of sensors and checks whether it's correct
+func TestCalculateSensorAverages(t *testing.T) {
+	sensorData := []SensorModelNoId{
+		{Humidity: 2, Temperature: 1, Pressure: 9},
+		{Humidity: 4, Temperature: 2, Pressure: 9},
+		{Humidity: 6, Temperature: 3, Pressure: 8},
+		{Humidity: 8, Temperature: 4, Pressure: 8},
+	}
+	trueAverages := AveragesResponse{
+		AveragePressure:    8.5,
+		AverageHumidity:    5,
+		AverageTemperature: 2.5,
+	}
+	averages := calculateSensorAverages(sensorData)
+	if averages != trueAverages {
+		t.Log(averages)
+		t.Fail()
+	}
+}
