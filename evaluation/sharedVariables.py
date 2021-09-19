f6360a24d8bb3cceec3986d7a490589f867742f2
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-09-19 20:29:55 +0200

    add papermill

--- evaluation/sharedVariables.py
+++ evaluation/sharedVariables.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import os
 
 project_id = '932771810925'  # Our project ID
 project = "dspj-315716"
 
-ip_client = "10.1.0.2"
-ip_server = "10.1.0.3"
-ip_orchestrator = "10.1.0.255"
+ip_client = "10.0.0.2"
+ip_server = "10.0.0.3"
+ip_orchestrator = "10.0.0.255"
 ip_seperator = "92.60.39.199"
 
 allPreKnownServers = [ip_client, ip_server]
@@ -47,3 +48,8 @@ def loadDataCSV(file):
     df.set_index('timestamp', inplace=True)
     df.index = pd.to_datetime(df.index)
     return df
+
+
+def ensureDirectory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
