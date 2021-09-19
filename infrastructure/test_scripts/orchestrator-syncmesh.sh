7a025fce6eed61bcd2add21de747326b15cbdbe1
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-09-19 21:51:15 +0200

    variable in syncmesh test

--- infrastructure/test_scripts/orchestrator-syncmesh.sh
+++ infrastructure/test_scripts/orchestrator-syncmesh.sh
@@ -20,50 +20,43 @@ seperate
 
 # Collect
 echo "Scenario: Collect - 1 day"
-ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions 20 --requestType "collect" --startTime "2017-07-31T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
+COMMAND=
+ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions'$REPETITIONS' --requestType "collect" --startTime "2017-07-31T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
 
-sleep $SLEEP_TIME
 seperate
 
 echo "Scenario: Collect - 7 day"
-ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions 20 --requestType "collect" --startTime "2017-07-24T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
+ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions '$REPETITIONS' --requestType "collect" --startTime "2017-07-24T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
 
-sleep $SLEEP_TIME
 seperate
 
 echo "Scenario: Collect - 14 day"
-ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions 20 --requestType "collect" --startTime "2017-07-17T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
+ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions '$REPETITIONS' --requestType "collect" --startTime "2017-07-17T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
 
-sleep $SLEEP_TIME
 seperate
 
 echo "Scenario: Collect - 30 day"
-ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions 20 --requestType "collect" --startTime "2017-06-30T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
+ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions '$REPETITIONS' --requestType "collect" --startTime "2017-06-30T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
 
-sleep $SLEEP_TIME
 seperate
 
 # Aggregate 
 echo "Scenario: Aggregate - 1 day"
-ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions 20 --requestType "aggregate" --startTime "2017-07-31T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
+ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions '$REPETITIONS' --requestType "aggregate" --startTime "2017-07-31T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
 
-sleep $SLEEP_TIME
 seperate
 
 echo "Scenario: Aggregate - 7 day"
-ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions 20 --requestType "aggregate" --startTime "2017-07-24T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
+ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions '$REPETITIONS' --requestType "aggregate" --startTime "2017-07-24T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
 
-sleep $SLEEP_TIME
 seperate
 
 echo "Scenario: Aggregate - 14 day"
-ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions 20 --requestType "aggregate" --startTime "2017-07-17T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
+ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions '$REPETITIONS' --requestType "aggregate" --startTime "2017-07-17T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
 
-sleep $SLEEP_TIME
 seperate
 
 echo "Scenario: Aggregate - 30 day"
-ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions 20 --requestType "aggregate" --startTime "2017-06-30T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
+ssh -o StrictHostKeyChecking=no $CLIENT_IP 'python3 /test.py --repetitions '$REPETITIONS' --requestType "aggregate" --startTime "2017-06-30T00:00:00Z" --endTime "2017-07-31T23:59:59Z"'
 
-sleep $SLEEP_TIME
 seperate
