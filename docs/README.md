160b796b2f486e18bc3894ef13a4b68518e0ae51
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-09-30 22:42:20 +0200

    clean up

--- docs/README.md
+++
@@ -1,4 +0,0 @@
-# Welcome to the syncmesh wiki!
-
-Here you will find all kinds of information regarding the setup, usage and evaluation of the syncmesh distributed storage project.
-
--- logs/test.py
+++
@@ -1,9 +0,0 @@
-from gcp_flowlogs_reader import Reader
-
-credentials_path = './../infrastructure/credentials.json'
-ip_set = set()
-for flow_record in Reader(service_account_json=credentials_path):
-    ip_set.add(flow_record.src_ip)
-    ip_set.add(flow_record.dest_ip)
-
-print(len(ip_set))
